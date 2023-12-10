93efd1b42df2200a1b0cf6550b6815b413a691b4
danielalejandrojaen <danielalejandrojaen@gmail.com>
2023-12-10 16:08:05 -0600

    adjusted some user profile stuff

--- client/src/components/userProfile.tsx
+++ client/src/components/userProfile.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useLoaderData } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import CardTitle from 'react-bootstrap/esm/CardTitle';
 
 const UserProfile = () => {
     const [selectedUser, setselectedUser] = useState({});
@@ -8,14 +10,27 @@ const UserProfile = () => {
     console.log('userLoader', currentUser);
     return (
         <div className="user-main">
-            <div className="user-profile-card">
+            <Card style={{ width: '20rem' }}>
+                <div className="user-main-card">
+                    <div className="user-pfp">
+                        <img src={currentUser.profileImgUrl} alt="user profile image" />
+                    </div>
+                </div>
+                <Card.Body>
+                    <Card.Title>{currentUser.username}</Card.Title>
+                    <Card.Text>
+                        {currentUser.bio || 'No bio yet'}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            {/* <div className="user-profile-card">
                 <div className="user-profile-image">
                     <img src={currentUser.profileImgUrl} alt="user profile image" />
                 </div>
                 <div className="user-profile-info">
                     <h1>{currentUser.username}</h1>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 };
--- server/index.ts
+++ server/index.ts
@@ -7,5 +7,5 @@ dotenv.config()
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`)
+  console.log(`Server is listening on http://localhost:${PORT}`)
 })
